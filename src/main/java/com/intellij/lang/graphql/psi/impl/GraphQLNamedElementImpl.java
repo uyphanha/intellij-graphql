d74c7b137ff5fbaa976cab7172ee7544c18ac659
Alec Strong <astrong@squareup.com>
2017-10-02 15:17:59 -0400

    Switch to gradle

--- src/main/java/com/intellij/lang/graphql/psi/impl/GraphQLNamedElementImpl.java
+++ src/main/java/com/intellij/lang/graphql/psi/impl/GraphQLNamedElementImpl.java
@@ -1,14 +1,27 @@
 package com.intellij.lang.graphql.psi.impl;
 
-import com.intellij.lang.annotation.AnnotationHolder;
-import com.intellij.lang.graphql.psi.GraphQLIdentifier;
-import com.intellij.lang.graphql.psi.GraphQLNamedElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.lang.graphql.psi.GraphQLNamedElement;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class GraphQLNamedElementImpl extends GraphQLElementImpl implements GraphQLNamedElement {
   public GraphQLNamedElementImpl(@NotNull ASTNode node) {
     super(node);
+  }
+
+  @Override public String getName() {
+    PsiElement identifier = getNameIdentifier();
+    if (identifier == null) return null;
+
+    ASTNode identifierNode = identifier.getNode();
+    if (identifierNode == null) return null;
+
+    return identifierNode.getText();
+  }
+
+  @Override public PsiElement setName(@NotNull String newName) throws IncorrectOperationException {
+    throw new IncorrectOperationException("GraphQLNamedElement does not support setName");
   }
 }
